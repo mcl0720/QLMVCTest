9f0a4214d2211fba7c0a3758dde2a6c0483edd30
mcl0720 <mcl0720>
2013-09-10 15:30:28 -0400

    Another round of updates for mockups.

---
+++ MvcTest/Controllers/CompletedController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTest.Controllers
+{
+    public class CompletedController : Controller
+    {
+        //
+        // GET: /Completed/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Survey()
+        {
+            return View();
+        }
+
+        public ActionResult Vote()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MvcTest/Controllers/InProgressController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTest.Controllers
+{
+    public class InProgressController : Controller
+    {
+        //
+        // GET: /InProgress/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Survey()
+        {
+            return View();
+        }
+
+        public ActionResult Vote()
+        {
+            return View();
+        }
+
+    }
+}
